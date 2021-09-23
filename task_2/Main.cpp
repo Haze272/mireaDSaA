7d7b54fac133659dc020e31e62529d0137943b08
Stepan Berkunov <55670335+Haze272@users.noreply.github.com>
2021-09-23 23:46:47 +0300

    Built hashTable
    С этим блять можно работать

--- task_2/Main.cpp
+++ task_2/Main.cpp
@@ -1,18 +1,96 @@
 ﻿#include <iostream>
 #include <string>
-#include "HashTable.h"
-
+#include <list>
 
 using namespace std;
 
-int main() {
-	HashTable hashid;
+// Необходимо для поиска переменной по имени
+bool operator==(string left, const string right)
+{
+    return left == right;
+}
 
-	cout << hashid.hasher("stoypa", 100) << endl;     // 72
-	cout << hashid.hasher("apyots", 100) << endl;     // 72
-	cout << hashid.hasher("stoypas", 100) << endl;    // 87
-	cout << hashid.hasher("stoypasa", 100) << endl;   // 84
-	cout << hashid.hasher("marina", 100) << endl;     // 32
-	cout << hashid.hasher("gsmarina", 100) << endl;   // 50
-	return 0;
+size_t hasher(string id)
+{
+    // Если имя переменной составляет один символ - возвращается его код,
+    // умноженный на два
+    if (id.length() == 1)
+        return 2 * size_t(id[0]);
+
+    // Иначе возвращается сумма кодов первых двух символов
+    return size_t(id[0]) + size_t(id[1]);
+}
+
+// Класс "Хэш-таблица", основанная на методе цепочек
+// Метод цепочек заключается в следующем: таблица представляет собой массив
+// связных списков фиксированного размера. Вычисленный хэш-функцией хэш является
+// индексом в этом массиве списков. Известно, что список по этому индексу будет
+// содержать все идентификаторы, для которых функция вернула одинаовый хэш.
+// Осталось только найти идентификатор в данном списке и возвратить ссылку на
+// него.
+class HashTable
+{
+public:
+    static const size_t min_hash_value = int('A') + int('0');
+    static const size_t max_hash_value = int('z') + int('z');
+    static const size_t hash_table_size = max_hash_value - min_hash_value;
+
+public:
+    void add(const string id)
+    {
+        // Добавление идентификатора в список, расположенный в таблице по
+        // индексу, вычисленному хэш-функцией (с учётом смещения)
+        m_hash_table[hasher(id) - min_hash_value].push_back(id);
+    }
+
+    
+    string get(const string id_name)
+    {
+        // Сохраняется ссылка на список, в котором потенциально будет
+        // расположен идентификатор (для простоты)
+        list<string>& line = m_hash_table[hasher(id_name) - min_hash_value];
+
+        // Поиск идентификаторы в списке по имени
+        list<string>::iterator it = find(line.begin(), line.end(), id_name);
+
+        // Если идентификатор найден - возвращаем ссылку на него
+        return *it;
+    }
+
+private:
+    // Хэш-таблица - массив связных списков идентификаторов
+    list<string> m_hash_table[hash_table_size];
+};
+
+int main()
+{
+    
+    HashTable ht;
+
+    
+    ht.add("a");
+    ht.add("aa");
+    ht.add("if");
+    ht.add("fi");
+
+    cout << hasher("a") << endl;
+    cout << hasher("aa") << endl;
+    cout << hasher("if") << endl;
+    cout << hasher("fi") << endl;
+
+    cout << endl;
+
+    cout << int('a') << endl;
+    cout << int('A') << endl;
+    cout << int('0') << endl;
+    cout << int('z') << endl;
+
+    cout << endl;
+
+    cout << ht.get("a") << endl;
+    cout << ht.get("aa") << endl;
+    cout << ht.get("if") << endl;
+    cout << ht.get("fi") << endl;
+
+    return 0;
 }
