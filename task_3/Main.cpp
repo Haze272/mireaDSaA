d9af29a498f56a89efab86b3f909e83b8b3e8461
Stepan Berkunov <55670335+Haze272@users.noreply.github.com>
2021-11-06 18:57:33 +0300

    Done average method. Refactored into OOP style

--- task_3/Main.cpp
+++ task_3/Main.cpp
@@ -7,69 +7,88 @@ struct Node {
 
     Node *lNode, *rNode;
 };
-Node *tree;
 
-void push(double a, Node **t) {
-    if ((*t) == NULL) {
-        (*t) = new Node;
-        (*t)->info  = a;
-        (*t)->lNode = (*t)->rNode = NULL;
-        return;
+class BinaryTree {
+    double summary;
+    int qElem;
+
+public:
+    Node *tree;
+
+    BinaryTree() {
+        tree = NULL;
+        qElem = 0;
     }
 
-    if (a > (*t)->info) {
-        push(a, &(*t)->rNode);
-    } else {
-        push(a, &(*t)->lNode);
+    void push(double a, Node **curr) {
+        if (*curr == NULL) {
+            qElem++;
+            (*curr) = new Node;
+            (*curr)->info  = a;
+            (*curr)->lNode = (*curr)->rNode = NULL;
+            return;
+        }
+
+        if (a > (*curr)->info) {
+            push(a, &(*curr)->rNode);
+        } else {
+            push(a, &(*curr)->lNode);
+        }
     }
-}
 
-void printTree(Node *t, int u) {
-    if (t == NULL) return;
+    void printTree(Node *t, int u) {
+        if (t == NULL) return;
 
-    printTree(t->rNode, ++u);
+        printTree(t->rNode, ++u);
 
-    for (int i = 0; i < u; ++i) {
-        cout << "  ";
+        for (int i = 0; i < u; ++i) {
+            cout << "  ";
+        }
+
+        cout << t->info << endl;
+        u--;
+
+        printTree(t->lNode, ++u);
     }
 
-    cout << t->info << endl;
-    u--;
+    double average() {
+        sumTree(tree, 0);
+        return summary/(double)qElem;
+    }
 
-    printTree(t->lNode, ++u);
-}
+private:
+    void sumTree(Node *t, int u) {
+        if (t == NULL) return;
 
-// TODO average function
-double summary;
-void sumTree(Node *t, int u) {
-    if (t == NULL) return;
+        sumTree(t->rNode, ++u);
 
-    sumTree(t->rNode, ++u);
+        summary += t->info;
+        u--;
 
-    summary += t->info;
-    u--;
-
-    sumTree(t->lNode, ++u);
-}
+        sumTree(t->lNode, ++u);
+    }
+};
 
 // TODO deleteTree function
 
 int main() {
     int n; //Количество элементов
     double s; //Число, передаваемое в дерево
+    BinaryTree bt;
+
     cout << "Enter the quantity of elements: ";
-    cin >> n; //Вводим количество элементов
+    cin >> n; //Вводим количество элементов\
 
     for (int i = 0; i < n; ++i) {
         cout << "Enter the number  ";
         cin >> s; //Считываем элемент за элементом
 
-        push(s, &tree); //И каждый кладем в дерево
+        bt.push(s, &bt.tree); //И каждый кладем в дерево
     }
-    cout << "Entire tree\n";
-    printTree(tree,0);
 
-    cout << "\n---------Sum---------\n\n\n";
-    sumTree(tree, 0);
-    cout << summary;
+    cout << "Entire tree\n";
+    bt.printTree(bt.tree, 0);
+
+    cout << "Entire average\n";
+    cout << bt.average();
 }
