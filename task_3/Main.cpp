70765392f3936b1f28b33f8beb8542c1b272e9d3
Stepan Berkunov <55670335+Haze272@users.noreply.github.com>
2021-11-07 01:27:40 +0300

    Totally done

--- task_3/Main.cpp
+++ task_3/Main.cpp
@@ -9,20 +9,12 @@ struct Node {
 };
 
 class BinaryTree {
-    double summary;
-    int qElem;
 
 public:
     Node *tree;
 
-    BinaryTree() {
-        tree = NULL;
-        qElem = 0;
-    }
-
     void push(double a, Node **curr) {
         if (*curr == NULL) {
-            qElem++;
             (*curr) = new Node;
             (*curr)->info  = a;
             (*curr)->lNode = (*curr)->rNode = NULL;
@@ -51,33 +43,62 @@ public:
         printTree(t->lNode, ++u);
     }
 
-    double average() {
-        sumTree(tree, 0);
-        return summary/(double)qElem;
+    double average(Node *t) {
+        double sum = sumElem(t);
+        int quantity = getQuantity(t);
+        return sum / quantity;
+    }
+
+    void treeClear(Node *t) {
+        if(t != nullptr){
+            if(t->lNode != nullptr)
+                treeClear(t->lNode);
+            if(t->rNode != nullptr)
+                treeClear(t->rNode);
+            delete t;
+        }
     }
 
 private:
-    void sumTree(Node *t, int u) {
-        if (t == NULL) return;
 
-        sumTree(t->rNode, ++u);
+    double sumElem(Node *t) {
+        int l, r;
+        if(t != nullptr){
+            l = (t->lNode  != nullptr) ? sumElem(t->lNode)  : 0;
+            r = (t->rNode != nullptr) ? sumElem(t->rNode) : 0;
+            return l + r + t->info;
+        }
+        return 0;
+    }
 
-        summary += t->info;
-        u--;
+    int getQuantity(Node *t) {
+        if (t->lNode == NULL && t->rNode == NULL)
+            return 1;
 
-        sumTree(t->lNode, ++u);
+        int left, right;
+
+        if (t->lNode != NULL)
+            left = getQuantity(t->lNode);
+        else
+            left = 0;
+
+        if (t->rNode != NULL)
+            right = getQuantity(t->rNode);
+        else
+            right = 0;
+
+        return left + right + 1;
     }
 };
-
-// TODO deleteTree function
 
 int main() {
     int n; //Количество элементов
     double s; //Число, передаваемое в дерево
-    BinaryTree bt;
+
 
     cout << "Enter the quantity of elements: ";
-    cin >> n; //Вводим количество элементов\
+    cin >> n; //Вводим количество элементов
+    BinaryTree bt;
 
     for (int i = 0; i < n; ++i) {
         cout << "Enter the number  ";
@@ -89,6 +110,14 @@ int main() {
     cout << "Entire tree\n";
     bt.printTree(bt.tree, 0);
 
-    cout << "Entire average\n";
-    cout << bt.average();
+    cout << "Entire average of left subtree\n";
+    cout << bt.average(bt.tree->lNode) << endl;
+
+    cout << "Entire average of right subtree\n";
+    cout << bt.average(bt.tree->rNode) << endl;
+
+    bt.treeClear(bt.tree);
+
+    cout << "Entire tree\n";
+    cout << bt.tree->info;
 }
