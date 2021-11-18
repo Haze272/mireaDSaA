528877eb15e9687972f41e134f518d2cc9989487
Stepan Berkunov <55670335+Haze272@users.noreply.github.com>
2021-11-19 01:03:22 +0300

    Created structure of 4 practice

--- task_4/Main.cpp
+++ task_4/Main.cpp
@@ -2,12 +2,36 @@
 
 using namespace std;
 
+struct Cell {
+    string name = "N/A";
+    string article = "N/A";
+
+    struct Cell* nextCell = nullptr;
+
+    bool isEmpty() const {
+        return article == "N/A" && name == "N/A";
+    }
+};
+
 class BinarySearchTree() {
+
+    Cell* root;
 public:
-    
+    BinarySearchTree() {
+        root = new root();
+    }
+
+    void insertElem() {
+        
+    }
+    Cell* findElem() {}
+    void deleteElem() {}
+    void printElem() {}
+
 }
 
 int Main() {
+    BinarySearchTree bTree;
 
 
     return 0;
